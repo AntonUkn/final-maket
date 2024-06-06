0da0aa8ea9c931709c21e206ac8617ac7d9952ec
Антон Уткин <anton.ukn@.mail.ru>
2024-06-06 15:05:06 +0300

    commit

--- js/bundle.js
+++ js/bundle.js
@@ -1,2 +1,402 @@
-!function(e){var n={};function t(r){if(n[r])return n[r].exports;var o=n[r]={i:r,l:!1,exports:{}};return e[r].call(o.exports,o,o.exports,t),o.l=!0,o.exports}t.m=e,t.c=n,t.d=function(e,n,r){t.o(e,n)||Object.defineProperty(e,n,{enumerable:!0,get:r})},t.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},t.t=function(e,n){if(1&n&&(e=t(e)),8&n)return e;if(4&n&&"object"==typeof e&&e&&e.__esModule)return e;var r=Object.create(null);if(t.r(r),Object.defineProperty(r,"default",{enumerable:!0,value:e}),2&n&&"string"!=typeof e)for(var o in e)t.d(r,o,function(n){return e[n]}.bind(null,o));return r},t.n=function(e){var n=e&&e.__esModule?function(){return e.default}:function(){return e};return t.d(n,"a",n),n},t.o=function(e,n){return Object.prototype.hasOwnProperty.call(e,n)},t.p="",t(t.s=0)}([function(e,n,t){e.exports=t(1)},function(e,n,t){"use strict";t.r(n);t(2),t(3),t(4)},function(e,n,t){},function(e,n){window.addEventListener("DOMContentLoaded",(function(){!function(e,n,t,r){var o;e=window.matchMedia(e);var i=function(){return e.matches?(o=new Swiper(n,t),void(r&&r(o))):void(void 0!==o&&o.destroy(!0,!0))};e.addEventListener("change",i),i()}("(max-width: 768px)",".swiper",{width:240,spaceBetween:16,pagination:{el:".swiper-pagination",clickable:!0}})}));var t=document.querySelector(".cards-more"),r=Array.from(document.querySelectorAll(".swiper-slide")).slice(6);window.addEventListener("resize",(function(){r.forEach((function(e){window.innerWidth<769&&(e.classList.add("hidden"),t.innerHTML="Показать все",t.classList.remove("cards-more--close"))}))})),t.addEventListener("click",(function(){r.forEach((function(e){e.classList.toggle("hidden")})),window.innerWidth>768&&r.forEach((function(e){e.classList.contains("hidden")?(t.innerHTML="Показать все",t.classList.remove("cards-more--close"),t.classList.remove("btn-hide")):(t.innerHTML="Скрыть",t.classList.add("cards-more--close"),t.classList.add("btn-hide"))}))}))},function(e,n){var t=Array.from(document.querySelectorAll(".btn--icon-burger"));console.log(t),t.addEventListener("click",(function(){}))}]);
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./src/js/index.js":
+/*!*************************!*\
+  !*** ./src/js/index.js ***!
+  \*************************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _scss_style_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../scss/style.scss */ "./src/scss/style.scss");
+/* harmony import */ var _scss_style_scss__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_scss_style_scss__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _js_swiper_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../js/swiper.js */ "./src/js/swiper.js");
+/* harmony import */ var _js_swiper_js__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_js_swiper_js__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _js_pop_up_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../js/pop-up.js */ "./src/js/pop-up.js");
+/* harmony import */ var _js_pop_up_js__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_js_pop_up_js__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var _js_modal_feedback_js__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../js/modal-feedback.js */ "./src/js/modal-feedback.js");
+/* harmony import */ var _js_info_section_js__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ../js/info-section.js */ "./src/js/info-section.js");
+/* harmony import */ var _js_info_section_js__WEBPACK_IMPORTED_MODULE_4___default = /*#__PURE__*/__webpack_require__.n(_js_info_section_js__WEBPACK_IMPORTED_MODULE_4__);
+
+
+
+
+
+
+/***/ }),
+
+/***/ "./src/js/info-section.js":
+/*!********************************!*\
+  !*** ./src/js/info-section.js ***!
+  \********************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+var btnMoreText = document.querySelector('.info-section__btn');
+var infoText = Array.from(document.querySelectorAll('.info-section__text-elem'));
+var infoDesktop = document.querySelector('.info-section__text-desktop');
+var infoSection = document.querySelector('.info-section');
+console.log(infoText);
+console.log(btnMoreText);
+console.log(infoDesktop);
+
+function openText() {
+  infoText.forEach(function (item) {
+    item.classList.toggle('info-section__hidden');
+  });
+  infoDesktop.classList.toggle('info-section__desktop-hidden');
+  btnText();
+}
+
+function btnText() {
+  infoText.forEach(function (item) {
+    if (item.classList.contains("info-section__hidden")) {
+      btnMoreText.innerHTML = "Показать все";
+    } else {
+      btnMoreText.innerHTML = "Скрыть";
+    }
+  });
+}
+
+btnMoreText.addEventListener('click', openText);
+
+/***/ }),
+
+/***/ "./src/js/modal-feedback.js":
+/*!**********************************!*\
+  !*** ./src/js/modal-feedback.js ***!
+  \**********************************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _js_pop_up_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../js/pop-up.js */ "./src/js/pop-up.js");
+/* harmony import */ var _js_pop_up_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_js_pop_up_js__WEBPACK_IMPORTED_MODULE_0__);
+
+
+/***/ }),
+
+/***/ "./src/js/pop-up.js":
+/*!**************************!*\
+  !*** ./src/js/pop-up.js ***!
+  \**************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+var btnBurger = document.querySelector('.btn--icon-burger');
+var btnClose = document.querySelector('.btn--icon-hide');
+var popUp = document.getElementById('pop-up');
+var blur = document.getElementById('blur');
+var btnCall = document.querySelectorAll('.btn--icon-phone');
+var modalFeedback = document.getElementById('feedback');
+var modalBtnClose = document.querySelector('.modal-feedback__btn-hide');
+var btnMessage = document.querySelectorAll('.btn--icon-chat');
+var modalCall = document.getElementById('modal-call');
+var modalCallBtnClose = document.querySelector('.modal-call__btn-hide');
+
+function openPopUp() {
+  popUp.classList.remove('pop-up--hidden');
+  blur.classList.add('blur');
+  modalFeedback.classList.add('modal-feedback--hidden');
+  modalCall.classList.add('modal-feedback--hidden');
+}
+
+function closePopUp() {
+  popUp.classList.add('pop-up--hidden');
+  blur.classList.remove('blur');
+}
+
+btnBurger.addEventListener('click', openPopUp);
+btnClose.addEventListener('click', closePopUp);
+document.addEventListener('keydown', function (event) {
+  if (event.code === 'Escape') {
+    popUp.classList.add('pop-up--hidden');
+    modalFeedback.classList.add('modal-feedback--hidden');
+    modalCall.classList.add('modal-feedback--hidden');
+    popUp.style.opacity = 1;
+    blur.classList.remove('blur');
+  }
+});
+
+function openModalCall() {
+  modalCall.classList.remove('modal-feedback--hidden');
+  popUp.classList.add('pop-up--hidden');
+  blur.classList.add('blur');
+  modalFeedback.classList.add('modal-feedback--hidden');
+
+  if (innerWidth > 1119) {
+    popUp.style.opacity = 0.1;
+  }
+}
+
+function closeModalCall() {
+  modalCall.classList.add('modal-feedback--hidden');
+  blur.classList.remove('blur');
+
+  if (innerWidth > 1119) {
+    popUp.style.opacity = 1;
+  }
+}
+
+function openModalFeedback() {
+  modalFeedback.classList.remove('modal-feedback--hidden');
+  popUp.classList.add('pop-up--hidden');
+  blur.classList.add('blur');
+  modalCall.classList.add('modal-feedback--hidden');
+
+  if (innerWidth > 1119) {
+    popUp.style.opacity = 0.1;
+  }
+}
+
+function closeModalFeedback() {
+  modalFeedback.classList.add('modal-feedback--hidden');
+  blur.classList.remove('blur');
+
+  if (innerWidth > 1119) {
+    popUp.style.opacity = 1;
+  }
+}
+
+btnMessage[0].addEventListener('click', openModalCall);
+btnMessage[1].addEventListener('click', openModalCall);
+modalCallBtnClose.addEventListener('click', closeModalCall);
+btnCall[0].addEventListener('click', openModalFeedback);
+btnCall[1].addEventListener('click', openModalFeedback);
+modalBtnClose.addEventListener('click', closeModalFeedback);
+
+/***/ }),
+
+/***/ "./src/js/swiper.js":
+/*!**************************!*\
+  !*** ./src/js/swiper.js ***!
+  \**************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+var MyswiperPrices;
+var MyswiperTechnique;
+var MyswiperBrands;
+var swiperBrands = document.querySelector('.swiper');
+var swiperPrices = document.querySelector('.prices');
+var swiperTechnique = document.querySelector('.swiper-technique');
+
+var createSwiper = function createSwiper(element, width) {
+  return new Swiper(element, {
+    width: width,
+    spaceBetween: 16,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    }
+  });
+};
+
+if (window.innerWidth < 769) {
+  MyswiperPrices = createSwiper(swiperPrices, 260);
+  MyswiperTechnique = createSwiper(swiperTechnique, 240);
+  MyswiperBrands = createSwiper(swiperBrands, 240);
+}
+
+var swiperBreakpoints = function swiperBreakpoints(element) {
+  window.addEventListener('resize', function () {
+    if (window.innerWidth > 768) {
+      element.destroy(false, true);
+    }
+  });
+};
+
+swiperBreakpoints(MyswiperPrices);
+swiperBreakpoints(MyswiperTechnique);
+swiperBreakpoints(MyswiperBrands); //Кнопка показать/скрыть..............
+
+var btnCardsBrand = document.querySelector('.cards-more');
+var cardsBrand = Array.from(document.querySelectorAll('.card-brand'));
+var cardsTechnique = Array.from(document.querySelectorAll('.card-technique'));
+var arrCardsBrand = cardsBrand.slice(6);
+var btnCardsTechnique = document.querySelector('.cards-technique');
+var arrCardsTechnique = cardsTechnique.slice(3);
+
+function openCardsBrands() {
+  arrCardsBrand.forEach(function (item) {
+    item.classList.toggle('hidden');
+  });
+  btnText();
+}
+
+function btnText() {
+  if (window.innerWidth > 768) {
+    arrCardsBrand.forEach(function (item) {
+      if (item.classList.contains('hidden')) {
+        btnCardsBrand.innerHTML = 'Показать все';
+        btnCardsBrand.classList.remove('cards-more--close');
+        btnCardsBrand.classList.remove('btn-hide');
+      } else {
+        btnCardsBrand.innerHTML = 'Скрыть';
+        btnCardsBrand.classList.add('cards-more--close');
+        btnCardsBrand.classList.add('btn-hide');
+      }
+    });
+  }
+}
+
+window.addEventListener('resize', function () {
+  arrCardsBrand.forEach(function (item) {
+    if (window.innerWidth < 769) {
+      item.classList.add('hidden');
+      btnCardsBrand.innerHTML = 'Показать все';
+      btnCardsBrand.classList.remove('cards-more--close');
+    }
+  });
+});
+
+function openCardsTechnique() {
+  arrCardsTechnique.forEach(function (item) {
+    item.classList.toggle('card-technique--hidden');
+  });
+  btnTextTechnique();
+}
+
+function btnTextTechnique() {
+  if (window.innerWidth > 768) {
+    arrCardsTechnique.forEach(function (item) {
+      if (item.classList.contains('card-technique--hidden')) {
+        btnCardsTechnique.innerHTML = 'Показать все';
+        btnCardsTechnique.classList.remove('cards-more--close');
+        btnCardsTechnique.classList.remove('btn-hide');
+      } else {
+        btnCardsTechnique.innerHTML = 'Скрыть';
+        btnCardsTechnique.classList.add('cards-more--close');
+        btnCardsTechnique.classList.add('btn-hide');
+      }
+    });
+  }
+}
+
+window.addEventListener('resize', function () {
+  arrCardsTechnique.forEach(function (item) {
+    if (window.innerWidth < 769) {
+      item.classList.add('card-technique--hidden');
+      btnCardsTechnique.innerHTML = 'Показать все';
+      btnCardsTechnique.classList.remove('cards-more--close');
+    }
+  });
+});
+btnCardsTechnique.addEventListener('click', openCardsTechnique);
+btnCardsBrand.addEventListener('click', openCardsBrands);
+
+/***/ }),
+
+/***/ "./src/scss/style.scss":
+/*!*****************************!*\
+  !*** ./src/scss/style.scss ***!
+  \*****************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+
+/***/ }),
+
+/***/ 0:
+/*!*******************************!*\
+  !*** multi ./src/js/index.js ***!
+  \*******************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! ./src/js/index.js */"./src/js/index.js");
+
+
+/***/ })
+
+/******/ });
 //# sourceMappingURL=bundle.js.map
